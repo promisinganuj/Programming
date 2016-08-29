8d4822fea09eaba66ef473f6acce16324bf7bf37
Anuj Parashar <promisinganuj@gmail.com>
2016-08-29 13:33:55 +1000

    Create finding_consecutive_dates.sql
    SQL to find n (5 in this case) consecutive dates.

---
+++ SQL/finding_consecutive_dates.sql
@@ -0,0 +1,45 @@
+create table tbl_5_cons_dates
+(	trx_date date, 
+	id number
+);
+
+SET DEFINE OFF;
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('05/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('10/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('11/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('12/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('13/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('14/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('15/JAN/16','DD/MON/RR'),2);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('16/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('16/JAN/16','DD/MON/RR'),2);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('17/JAN/16','DD/MON/RR'),2);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('18/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('18/JAN/16','DD/MON/RR'),2);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('19/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('19/JAN/16','DD/MON/RR'),2);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('20/JAN/16','DD/MON/RR'),3);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('21/JAN/16','DD/MON/RR'),1);
+Insert into TBL_5_CONS_DATES (TRX_DATE,ID) values (to_date('22/JAN/16','DD/MON/RR'),1);
+
+commit;
+
+select trx_date
+      ,id
+ from (
+        select c.*
+              ,count(*) over (partition by id, grp) grp_mem_cnt
+          from (
+                 select b.*
+                       ,trx_date - to_date('01-Jan-2016', 'DD-Mon-YYYY') - row_number() over (partition by id order by trx_date) grp
+                 from (
+                        select a.*,
+                               nvl(lag(trx_date) over (partition by id order by trx_date), trx_date) prev_trx_date,
+                               nvl(lead(trx_date) over (partition by id order by trx_date), trx_date) next_trx_date
+                          from tbl_5_cons_dates a
+                      ) b
+                 where trx_date - prev_trx_date = 1
+                    or next_trx_date - trx_date = 1
+               ) c
+      ) d
+ where grp_mem_cnt = 5;
